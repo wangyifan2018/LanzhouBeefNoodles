20e564757a5a6a4e0d093c223875fa96dd1cea76
Yifan <372104922@qq.com>
2022-05-07 15:25:17 +0800

    feat : initialize database

---
+++ LanzhouBeefNoodles/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LanzhouBeefNoodles.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Noodle> Noodles { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+    }
+}
--- LanzhouBeefNoodles/Startup.cs
+++ LanzhouBeefNoodles/Startup.cs
@@ -4,24 +4,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace LanzhouBeefNoodles
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); 
             services.AddMvc();
-            services.AddTransient<INoodleRepository, MockNoodleRepository>();
+            services.AddTransient<INoodleRepository, NoodleRepository>();
             //services.AddSingleton;
             //services.AddScoped;
-            services.AddTransient<IFeedbackRepository, MockFeedbackRepository>();
+            services.AddTransient<IFeedbackRepository, FeedbackRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
